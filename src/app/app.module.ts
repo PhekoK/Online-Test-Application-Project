93ce4329fb1469c1b69e66dd398095e8c59341f1
Karabo Pheko <216021429@student.uj.ac.za>
2021-06-14 01:37:17 +0200

    Added login, registration, quiz & dashboard pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { ReviewComponent } from './review/review.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    QuizComponent,
+    ReviewComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
