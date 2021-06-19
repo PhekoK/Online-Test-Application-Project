90b273d7584d06326b3786d82284f4aefdd2ddc3
Karabo Pheko <216021429@student.uj.ac.za>
2021-06-19 17:24:55 +0200

    Fixed Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../models/user';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'], 
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User();
+
+  userList: Array<User>  = [];
+
+  constructor(private _router: Router, private _userService: UserService) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+
+    this._userService.getUser().subscribe(result => {
+      console.log(result);
+      this.userList = result;
+      for (let i = 0; i < this.userList.length; i++) {
+        if (this.userList[i].username == this.user.username) {
+          localStorage.setItem('isLoggedIn', "true");
+          localStorage.setItem('user', this.user.username);
+          this._router.navigate(['/dashboard']);
+        }
+      }
+    })
   }
 
 }
